62d03147b1de8a601b16610d202e36985f390012
sumansharma01 <suman.sharma0331@gmail.com>
2021-03-13 22:49:24 +0530

    initialized first commit

---
+++ src/test/java/com/example/youtube/demo/YoutubeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.youtube.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
